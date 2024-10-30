5982092a1275bfa189d1fe5fb44d9a3e90496604
henritresor <shimwamanahenritresor@gmail.com>
2024-10-30 11:34:00 +0200

    fetched bf members

--- src/controller/bf.controller.ts
+++ src/controller/bf.controller.ts
@@ -3,6 +3,8 @@ import Bf from '../model/bf.model';
 import Group from '../model/group.model';
 import { ValidateToken } from '../utils/password.utils';
 import asyncWrapper from '../middlewares/AsyncWrapper';
+import user_bfModel from '../model/user_bf.model';
+import mongoose from 'mongoose';
 
 
 export const createBf = asyncWrapper(async (req: Request, res: Response) => {
@@ -62,11 +64,84 @@ export const getGroupBf = asyncWrapper(async (req: Request, res: Response) => {
         }
 
         res.status(200).json({
-            status:true,
+            status: true,
             bf: fund
         });
     } catch (error) {
         console.error("Error fetching fund:", error);
         res.status(500).json({ error: "Error fetching fund" });
     }
+})
+
+export const getBfMembers = asyncWrapper(async (req, res) => {
+    const members = await user_bfModel.aggregate([
+        {
+            $match: {
+                bf_id: new mongoose.Types.ObjectId(req.params.bf_id),
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "userId",
+                foreignField: "_id",
+                as: "member"
+            }
+        },
+        {
+            $lookup: {
+                from: "bfs",
+                localField: "bf_id",
+                foreignField: "_id",
+                as: "bf"
+            }
+        },
+        {
+            $unwind: {
+                path: "$member",
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $unwind: {
+                path: "$bf",
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $project: {
+                _id: 1,
+                user: "$member",
+                bf: "$bf",
+                role: 1,
+                createdAt: 1
+            }
+        }
+    ]);
+
+
+    res.status(200).json({
+        status: true,
+        members
+    })
+})
+
+
+export const updateBfUser = asyncWrapper(async (req, res) => {
+
+
+    const bfUser = await user_bfModel.findOne({ userId: req.body.userId, bf_id: req.body.bf_id })
+    if (!bfUser) {
+        let newBfUser = await user_bfModel.create({
+            userId: req.body.userId,
+            bf_id: req.body.bf_id,
+            role: "principal"
+        })
+    }
+
+    const updatedBfUser = await user_bfModel.findOneAndUpdate({ userId: req.body.userId, bf_id: req.body.bf_id }, { ...req.body }, { new: true })
+    res.status(200).json({
+        status: true,
+        bf_user: updatedBfUser
+    })
 })
--- src/routes/bf.routes.ts
+++ src/routes/bf.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createBf, getGroupBf } from "../controller/bf.controller";
+import { createBf, getBfMembers, getGroupBf, updateBfUser } from "../controller/bf.controller";
 
 const router = Router()
 
+router.get('/members/:bf_id', getBfMembers)
 router.post('/', createBf)
 router.get('/:groupId', getGroupBf)
+router.put('/user', updateBfUser)
 
 export default router
