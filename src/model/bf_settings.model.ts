0716e6074c5df67cf2d3ecbb5299151be221d16a
henritresor <shimwamanahenritresor@gmail.com>
2024-10-30 21:05:34 +0200

    added more fields to bf settings model

--- src/model/bf_settings.model.ts
+++ src/model/bf_settings.model.ts
@@ -1,13 +1,30 @@
-import mongoose, { Schema, model } from 'mongoose'
+import mongoose, { Schema, model } from 'mongoose';
+
+const FundBenefitsSchema = new Schema({
+    principal: { type: Number, default: 0 },
+    spouse: { type: Number, default: 0 },
+    children: { type: Number, default: 0 },
+    parents: { type: Number, default: 0 },
+    other: { type: Number, default: 0 },
+});
+
+const SubscriptionCostsSchema = new Schema({
+    youth: { type: Number, default: 0 },
+    children: { type: Number, default: 0 },
+    elders: { type: Number, default: 0 },
+});
 
 const BfSettingsSchema = new Schema({
-
     bf_id: { type: Schema.Types.ObjectId, ref: 'Bf', required: true },
-    max_beneficiaries: { type: Number, default: 1, required: true, min: 0 },
-    min_beneficiaries: { type: Number, default: 0, required: true, min: 1 },
+    maxBeneficiaries: { type: Number, default: 1, required: true, min: 0 },
+    minBeneficiaries: { type: Number, default: 1, required: true, min: 1 },
+    membership_fee: { type: Number, default: 0, required: true },
+    subscription_costs: { type: SubscriptionCostsSchema, default: {} },
+    fund_benefits: { type: FundBenefitsSchema, default: {} },
+    incident_contribution_fee: { type: Number, default: 0, required: true },
+    in_kind_support: { type: String, default: "" },
 }, {
-    timestamps: true
-})
+    timestamps: true,
+});
 
-
-export default model('Bf_settings', BfSettingsSchema)
+export default model('Bf_settings', BfSettingsSchema);
